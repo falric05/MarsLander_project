6e3b77e276695fdeb6124028fc99ee1d5deb88a0
falric05 <falco.riccardo@outlook.it>
2022-07-09 19:05:47 +0200

    comments minput

--- modules/minput.pl
+++ modules/minput.pl
@@ -1,16 +1,17 @@
 :- module(input, [zone/2, bl_landing_site/1, mars_zone/1, lander/3, g/1]).
 
-% The zone is 7000m wide and 3000m high.
+%%% The zone is 7000m wide and 3000m high.
 zone(7000, 3000).
 
-% The limit for the landing site of the shuttle must be at least
+%%% The limit for the landing site of the shuttle must be at least
 bl_landing_site(1000).
 
-% The number surfaceN of points used to draw the surface of Mars.
-% a couple of integers landX landY providing the coordinates of a ground point. 
-% By linking all the points together in a sequential fashion, you form the surface of Mars which is composed 
-% of several segments. 
-% For the first point, landX = 0 and for the last point, landX = 6999
+/* The number surfaceN of points used to draw the surface of Mars.
+ * a couple of integers landX landY providing the coordinates of a ground point. 
+ * By linking all the points together in a sequential fashion, you form the surface of Mars which is composed 
+ * of several segments. 
+ * For the first point, landX = 0 and for the last point, landX = 6999
+ */
 mars_zone([
      surface(0, 1500),
      surface(1000, 2000),
@@ -20,8 +21,9 @@ mars_zone([
      surface(6999, 1000)
 ]).
 
-% Start position of the lander and amount of carburants in litres
+%%% Start position of the lander and amount of carburants in litres
 lander(500, 2000, 4500).
 
-% The game simulates a free fall without atmosphere. Gravity on Mars is 3.711 m/s².
+%%% The game simulates a free fall without atmosphere. Gravity on Mars is 3.711 m/s².
+%%% TODO: try to fix the language grammar for float number 
 g(3.711).
