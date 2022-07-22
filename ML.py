15be0cf22ff5b4d97c4dad6895c62348321564cb
falric05 <falco.riccardo@outlook.it>
2022-07-23 00:29:49 +0200

    marker lander

--- ML.py
+++ ML.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation as anm 
+from svgpathtools import svg2paths
+from svgpath2mpl import parse_path
 from turtle import color
 import os
 
@@ -8,7 +10,15 @@ from utils.args import parse_args
 
 POINTS = []
 ML = []
+lander_marker = None
 
+
+def __getLanderMarker():
+    lander_path = os.path.join('MarsLander_project', 'Images', 'lander.svg')
+    assert(os.path.exists(lander_path))
+    lander_path, attributes = svg2paths(lander_path)
+    lander_marker = parse_path(attributes[0]['d'])
+    return lander_marker
 
 def __readFile(in_file):
     assert(os.path.exists(in_file))
@@ -29,10 +39,12 @@ def main(args):
         ax.set_xlim([0, 7000])
         ax.plot([p[0] for p in POINTS],
                 [p[1] for p in POINTS], color='r')
-        ax.scatter(ML[0], ML[1], marker='*', s=85, color='purple')
+        ax.scatter(ML[0], ML[1], marker=lander_marker, s=1500, color='purple')
+        ax.set_title(args.data)
 
     file = os.path.join('MarsLander_project', 'input', args.data + '.txt')
     POINTS, ML = __readFile(file)
+    lander_marker = __getLanderMarker()
 
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches(8,4)
