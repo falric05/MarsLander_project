429c207300065a8ef7d5f951ce11e1213ac2da41
falric05 <falco.riccardo@outlook.it>
2022-08-22 11:07:54 +0200

    ML.py comments

--- ML.py
+++ ML.py
@@ -50,37 +50,43 @@ def __readFile(in_file):
     return points, ml, flatArea
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
+### This function returns next game vector
 def next_round(ml, r, p):
+    ### converts degree in radians
     r1 = (r + 90) * np.pi/180
-    # r1 = r * np.pi/180
+    ### takes position and speed of the ship
     s0 = [ml[0], ml[1]]
     v0 = [ml[2], ml[3]]
+    ### takes fuel
     f = ml[4]
+    #
     f1 = 0
-    if f > p:
+    if f > p:           ### if the ship has still fuel
         amod = p
         f1 += f - p
     elif f > 0:
         amod = f
-    else:
+    else:               ### otherwise
         amod = 0
+    ### decomposes the acceleration in components subtracting 
+    ### mars gravity to y component
     a = [amod*np.cos(r1), amod*np.sin(r1) - 3.711]
     print('acc:',a)
+    ### returns new game vector
     return [
-        int(.5 * a[0] + v0[0] + s0[0]),     # same x as before
+        int(.5 * a[0] + v0[0] + s0[0]),     # new x position
         int(.5 * a[1] + v0[1] + s0[1]),     # new y position
-        int(a[0] + v0[0]),                  # 0 horizontal speed
+        int(a[0] + v0[0]),                  # new horizontal speed
         int(a[1] + v0[1]),                  # new vertical speed
-        f1,                            # new amount of liters
+        f1,                                 # new amount of liters
         r,
         p
     ]
 
 ### This function return the landing status
-### @retval 1  for correct landing
-###         0  for in game status
-###         -1 for destructive landing
+### @retval  1  for correct landing
+###          0  for in game status
+###         -1  for destructive landing
 def land_status(ps, ml, flatArea):
     if (ml[0] <= 0 or ml[1] <= 0) or (ml[0] >= MAX_X or ml[1] >= MAX_Y):
         return -1
@@ -114,22 +120,29 @@ def land_status(ps, ml, flatArea):
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 def main(args):
+    ### animate surface is used to plot a single frame of the game, taking into account
+    ### the i-th ML vector line 
     def __animate_surface(i):
         ax.clear()
         ax.set_ylim([0, 3200])
         ax.set_xlim([0, 7000])
         ax.set_facecolor('black')
+        ### plots the mars surface points
         ax.plot([p[0] for p in POINTS],
                 [p[1] for p in POINTS], color='r')
+        ### get the lander marker considering its rotation
         lander_marker = __getLanderMarker(ML[i][5])
+        ### plots the marker
         ax.scatter(ML[i][0], ML[i][1], marker=lander_marker, 
                    s=lander_marker_size, color='w')
+        ### plots game info
         ax.text(0.05, 0.9, 't: '+str(i)+'\nhspeed: '+str(round(ML[i][2], 2))+'\nvspeed: '+str(round(ML[i][3], 2)),
                 horizontalalignment='left', verticalalignment='center', transform=ax.transAxes,
                 color='w')
         ax.text(0.75, 0.9, 'fuel: '+str(ML[i][4])+'\nrotate: '+str(ML[i][5])+'\npower: '+str(ML[i][6]),
                 horizontalalignment='left', verticalalignment='center', transform=ax.transAxes,
                 color='w')
+        ### if this is the last turn 
         if i == len(ML)-1:
             if F_land:
                 ax.text(0.05, 0.8, 'landing success!',
@@ -139,6 +152,7 @@ def main(args):
                 ax.text(0.05, 0.8, 'landing failed!',
                         horizontalalignment='left', verticalalignment='center', transform=ax.transAxes,
                         color='r')
+        ### set the title to the plot
         ax.set_title(args.data)
 
     ### read input file
@@ -155,10 +169,11 @@ def main(args):
     F_esc = False
     while t < 1000 and not F_esc:
         print('t=',t)
-        ### write game informations in the file mlander.pl
-        ### read from mlander
+
+        ### read from mlander.pl
         mlander_file = open(mlander_url, 'r')
         lines = mlander_file.readlines()
+        ### overwrite last line until game informations
         lines = lines[:-1]
         lines.append('lander('+\
             str(int(round(ML[-1][0], 0)))+', ' +\
@@ -169,34 +184,47 @@ def main(args):
             str(ML[-1][5])+', ' +\
             str(ML[-1][6])+').')
         mlander_file.close()
+        ### overwrite the file mlander.pl
         mlander_file = open(mlander_url,'w')
         mlander_file.writelines(lines)
         mlander_file.close()
+
         print(ML[-1])
 
         ### make prediction
         out_line = os.popen('swipl -s .\MarsLander_project\modules\msolve.pl -g predict').read().split()
+
         print(out_line)
+
+        ### extract new game actions
         r = int(out_line[0])
         p = int(out_line[1])
+        ### assert consistency of new actions
         assert((abs(r - ML[-1][5]) <= 15) and (abs(p- ML[-1][6]) <= 1))
 
-        ### print results
+        ### compute next round
         ML.append(next_round(ML[-1], r, p))
+        ### check the land status
         lnd_status = land_status(POINTS, ML[-1], flatArea)
+
         print("land status", lnd_status)
         print("")
+
         t+=1
-        if lnd_status == 1:
+        if lnd_status == 1:             ### if landing had success
             F_esc = True
             F_land = True
             ML[-1][1] = flatArea[2]
-        elif lnd_status == -1:
+        elif lnd_status == -1:          ### if landing failed
             F_land = False
             F_esc = True
     
+    ### if F_esc is still false it means that the lander is till flying
+    ### for the sake of consistency:
+    ###     not(F_esc) => not(F_land)
     if not F_esc:
         F_land = False
+    
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches(8,4)
     
