a5dd615534cf500dda6f4c40af86e287c2341bce
falric05 <falco.riccardo@outlook.it>
2022-08-22 11:11:08 +0200

    minor changes

--- utils/args.py
+++ utils/args.py
@@ -3,7 +3,7 @@ import argparse
 DEFAULT_INPUT = 'ins-1'
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='SWI Prolog .')
+    parser = argparse.ArgumentParser(description='Mars Lander - Episode 2 solved with SWI Prolog.')
 
     parser.add_argument('--data', required=True, type=str, help='name of txt data file')
     parser.add_argument('--plot', required=False, action="store_true", help='show final solutions plot')
