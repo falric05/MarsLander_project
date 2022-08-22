8dbf7f785d4511a7091eb066d47393d461be810a
falric05 <falco.riccardo@outlook.it>
2022-08-22 11:09:22 +0200

    strorage.py comments

--- utils/storage.py
+++ utils/storage.py
@@ -15,17 +15,20 @@ class StorageClass:
     def _root_dir(self) -> Path:
         return ROOT.joinpath(self._root_dir_name)
 
+    ### data folder directory
     def data_dir(self) -> Path:
         return ROOT.joinpath("data")
     
+    ### get data ins path
     def data_file_url(self, file_name: str) -> Path:
         assert isinstance(file_name, str)
         return self.data_dir().joinpath(f"{file_name}.txt")
 
+    ### out folder directory
     def out_dir(self) -> Path:
-        # return self._root_dir.joinpath("out/json")
         return ROOT.joinpath("out")
 
+    ### out gif file path
     def out_file_url(self, file_name: str, sub_dir: str = None) -> Path:
         assert isinstance(file_name, str)
         assert sub_dir is None or isinstance(sub_dir, str)
@@ -33,7 +36,6 @@ class StorageClass:
         partial_file_url = f"{sub_dir}/" if sub_dir is not None else ""
 
         self.out_dir().joinpath(partial_file_url).mkdir(exist_ok=True, parents=True)
-
         return self.out_dir().joinpath(f"{partial_file_url}{file_name}.gif")
 
 Storage = StorageClass()
